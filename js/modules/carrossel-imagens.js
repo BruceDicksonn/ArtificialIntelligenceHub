0b81e8f071441781273a8184c2229f70eb4c6566
BruceDicksonn <dicksonbruce1110@gmail.com>
2023-05-30 19:07:11 -0300

    Implementacao do carrossel concluida e finalizacao do scrollview suave tambem

--- js/modules/carrossel-imagens.js
+++ js/modules/carrossel-imagens.js
@@ -5,12 +5,18 @@ export default function initCarrossel() {
         const splide = new Splide('.splide', {
             type: 'fade',
             rewind: true,
-            arrows: false,
+            arrows: true,
             pagination: false,
             drag: false,
             speed: 1500,
             interval: 3500,
-            autoplay: true
+            breakpoints: {
+                600: {
+                    arrows: true,
+                    drag: true
+                }
+            }
+
         });
         
         splide.mount();
