fe758140f58c7cdf38b3466abb116325ab25769c
BruceDicksonn <dicksonbruce1110@gmail.com>
2023-05-30 19:15:34 -0300

    Caminhos dos arquivos modificados

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -2,28 +2,28 @@ export default function initScrollSuave(){
 
     const menu  = document.querySelector('.js-menu');
     const links = menu.querySelectorAll('a');
-    
+
     function navegar(event) {
         event.preventDefault();
-    
+
         const target = document.querySelector(this.hash);
         target.scrollIntoView({
             behavior: 'smooth',
             block: 'start'
         });
-    
+
     }
-    
+
     function addScrollEvent(){
         links.forEach(link => link.addEventListener('click', navegar));
     }
-    
+
     function init() {
         if(menu && links) {
             addScrollEvent();
         }
     }
-    
+
     init();    
 
 }
